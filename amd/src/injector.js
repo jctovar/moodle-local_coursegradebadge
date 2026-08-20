f593db156715abe1b4c94f445db4c3c57d9ac5d1
Juan Carlos Tovar Gomez <jctovar@gmail.com>
2026-08-20 11:29:55 -0600

    chore: cabeceras de licencia canonicas GPL v3 con titular FES Iztacala

--- amd/src/injector.js
+++ amd/src/injector.js
@@ -1,11 +1,24 @@
 // This file is part of Moodle - https://moodle.org/
 //
-// GPL v3 or later. See https://www.gnu.org/licenses/gpl-3.0.html.
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
 /**
  * Injects the course total grade badge into block_myoverview cards.
  *
  * @module local_coursegradebadge/injector
+ * @copyright 2026 FES Iztacala, UNAM — Psicología SUAyED
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GNU GPL v3 or later
  */
 define(['core/ajax', 'core/str', 'core/templates'], function(Ajax, Str, Templates) {
 
